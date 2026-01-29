1a460f5594d7e32b7f2d95b7c3bbf921e7be6973
kranthi <kranthireddy.mekala@gmail.com>
2026-01-29 15:39:02 +0530

    Profiles and Configuration

--- src/main/java/com/enterprise/platform/EnterpriseDataPlatformApplication.java
+++ src/main/java/com/enterprise/platform/EnterpriseDataPlatformApplication.java
@@ -1,11 +1,13 @@
-package org.enterprise;
+package com.enterprise.platform;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EnterpriseDataPlatformApplication {
     private static final Logger log = LoggerFactory.getLogger(EnterpriseDataPlatformApplication.class);
 
